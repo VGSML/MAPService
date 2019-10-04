2c273f55253304f550e8620884f4409b7a925c41
VGSML <gribanov.vladimir@gmail.com>
2019-10-04 21:19:46 +0300

    first working commit

--- OSMTS/docker-entrypoint.sh
+++ OSMTS/docker-entrypoint.sh
@@ -5,16 +5,32 @@ if [ ! -f /data/.initialized ]; then
     echo "/data/${OSM_IMPORTFILE} -C ${IMPORT_RAM_CACHE} ${IMPORT_PROCESS_NUM}" 
     export PGPASS=$POSTGRES_PASSWORD
     export PGPASSWORD=$POSTGRES_PASSWORD
-    exec osm2pgsql -d "${POSTGRES_DB}" --create --slim -G --hstore -H mapdb -U "${POSTGRES_USER}" \
-              --tag-transform-script /src/openstreetmap-carto/openstreetmap-carto.lua \ 
-              -C  $IMPORT_RAM_CACHE --number-process $IMPORT_PROCESS_NUM \
-              -S default.style -r pbf "/data/${OSM_IMPORTFILE}"
-    exec /src/openstreetmap-carto/scripts/get-shapefiles.py
-    exec cp /usr/local/etc/renderd.conf /data
-    exec cp /src/openstreetmap-carto/mapnik.xml /data
-    exec cp /etc/apache2/conf-available/mod_tile.conf /data/
-    exec cp /etc/apache2/sites-available/000-default.conf /data
-    touch /data/.initialized
+    #mkdir ~/data
+    #cd /data
+    #wget http://download.geofabrik.de/asia/azerbaijan-latest.osm.pbf
+    osm2pgsql -d "${POSTGRES_DB}" --create --slim -H mapdb -U "${POSTGRES_USER}" -G --hstore --tag-transform-script /src/openstreetmap-carto/openstreetmap-carto.lua -C  $IMPORT_RAM_CACHE --number-process $IMPORT_PROCESS_NUM -S /src/openstreetmap-carto/openstreetmap-carto.style "/data/${OSM_IMPORTFILE}"
+    echo "Shape file download"
+    /src/openstreetmap-carto/scripts/get-shapefiles.py
+    echo "Configure appach"
+    mkdir /var/lib/mod_tile
+    mkdir /var/run/renderd
+    echo "LoadModule tile_module /usr/lib/apache2/modules/mod_tile.so" > /etc/apache2/conf-available/mod_tile.conf
+    cp /data/apache_cfg.conf /etc/apache2/sites-available/000-default.conf
+    cp /data/renderd_cfg.conf /usr/local/etc/renderd.conf
+    a2enconf mod_tile
+    echo "copy file to /data volume"
+    cp /usr/local/etc/renderd.conf /data
+    cp /src/openstreetmap-carto/mapnik.xml /data
+    cp /etc/apache2/conf-available/mod_tile.conf /data/
+    cp /etc/apache2/sites-available/000-default.conf /data
+    service apache2 reload
+    service apache2 reload
+    echo "ok" > /data/.initialized
+    renderd -f -c /usr/local/etc/renderd.conf
+    cp /src/mod_tile/debian/renderd.init /etc/init.d/renderd
+    cp /src/mod_tile/debian/renderd.service /lib/systemd/system/
+    systemctl enable renderd
+    #exit 'ok'
 fi
 
-#exec "@"
+exec apachectl -D FOREGROUND
